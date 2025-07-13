01245b79fc75a9d58686f07586612de31be5d71b
shauryajain7 <shaurya40000@gmail.com>
2025-07-13 20:16:53 +0530

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+        <h1 className="text-2xl font-bold mb-6">COPO Home</h1>
+        <div className="p-4 flex flex-col items-center justify-center min-h-[50vh]">
+            <p className="text-xl text-center">Welcome to the COPO Service!</p>
+        </div>
+    </div>
+  );
+}
+
